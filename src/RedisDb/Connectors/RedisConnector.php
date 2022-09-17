a91cabcfd86ccc7144499fef8369cbc257bf875c
Ivan Voitovych <col606v@gmail.com>
2022-09-17 16:53:26 +0300

    RedisConnector: db index

--- src/RedisDb/Connectors/RedisConnector.php
+++ src/RedisDb/Connectors/RedisConnector.php
@@ -8,15 +8,20 @@ use RedisDb\Interfaces\IRedisConnector;
 class RedisConnector implements IRedisConnector
 {
     private static $Connections = [];
+    private int $dbIndex = 0;
+
+    public function __construct(int $dbIndex = 0)
+    {
+        $this->dbIndex = $dbIndex;
+    }
 
     public function get(): Redis
     {
-        $dbIndex = 5; // TODO: config, connection string
-        if (!isset(self::$Connections[$dbIndex])) {
-            self::$Connections[$dbIndex] = new \Redis();
-            self::$Connections[$dbIndex]->connect('127.0.0.1', 6379);
-            self::$Connections[$dbIndex]->select($dbIndex);
+        if (!isset(self::$Connections[$this->dbIndex])) {
+            self::$Connections[$this->dbIndex] = new \Redis();
+            self::$Connections[$this->dbIndex]->connect('127.0.0.1', 6379);
+            self::$Connections[$this->dbIndex]->select($this->dbIndex);
         }
-        return self::$Connections[$dbIndex];
+        return self::$Connections[$this->dbIndex];
     }
 }
